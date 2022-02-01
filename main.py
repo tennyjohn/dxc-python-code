d1a94819aba4790be665d33a1b2cfcf2ac3362aa
Joseph John <54770517+tennyjohn@users.noreply.github.com>
2022-02-01 12:54:44 +0530

    added comments for explaining the flow of code

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-## code for generating the fibanocii series till 8th term
+## code for generating the fibanocii series till 8th term.
+
+
+## declares two variables 'a' and 'b' and assigns 0 and 1 to them.
 
 a = 0
 b = 1
 
-## printing first two values
+## printing first values of a and b.
 
 print(a)
 print(b)
 
-## generating sequence from 2 to 8 and adding with previous terms to generate fibanocci series and prints the output.
+## generating sequence from 2 to 8.
 
 for i in range(2,8):
+
+## declares a new variable 'c' and assign the sum of 'a' and 'b' to 'c'.
+
   c = a + b
+
+## swapping the values
+## assigning previous value of 'b' to 'a' and 'c' to 'b' and prints the value of 'c'.
+
   a = b
   b = c
+
   print(c)
