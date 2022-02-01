c1958bf54c1002bb2d6a1e501219cce131a6283f
Joseph John <54770517+tennyjohn@users.noreply.github.com>
2022-02-01 12:45:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 
 a = 0
 b = 1
+
+## printing first two values
+
 print(a)
 print(b)
+
+## generating sequence from 2 to 8 and adding with previous terms to generate fibanocci series and prints the output.
+
 for i in range(2,8):
   c = a + b
   a = b
